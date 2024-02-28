9a67a17c032db65fb179b1c4c84d6fb80c3e10d5
arturszmer <arturszmer@gmail.com>
2024-02-28 17:42:55 +0100

    first

---
+++ src/main/java/com/eventpaiger/eventpaigergw/EventPaigerGwApplication.java
@@ -0,0 +1,13 @@
+package com.eventpaiger.eventpaigergw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPaigerGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPaigerGwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventpaiger/eventpaigergw/EventPaigerGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventpaiger.eventpaigergw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventPaigerGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
